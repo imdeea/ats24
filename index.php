91c9d0de4013cf8b243bc43810326b7940a6a328
Andreea Butiu <deea.b22@gmail.com>
2022-05-10 10:42:55 +0300

    initial

---
+++ index.php
@@ -0,0 +1,301 @@
+<?
+$title = 'ATS - Converters and Fabricators | Tapes, Coated Fabrics, Films';
+$meta_descr = 'ATS are a high performance converter, fabricator and distributor of pressure sensitive tapes, elastometers, coated fabrics and plastic films. Contact us';
+include 'inc_header.php';
+?>
+
+	<div class="tp-banner-container rs_fullwidth">
+		<div class="tp-banner">
+			<ul>
+				
+				<!-- SLIDE 1 -->
+				<li data-transition="fade" data-masterspeed="500" data-slotamount="7" data-delay="8000" data-title="Slide 1">
+					<!-- Background Image -->
+				   <img src="images/sample_1.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <div class="bg-overlay op6"></div>
+
+
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="-75" 
+						data-speed="600" data-start="600"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h3 class="title-slider-small uppercased mb10">Advanced Technology Supply</h3>
+					</div>
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="35" 
+						data-speed="700" data-start="800"
+						data-endspeed="500" data-end="7950"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large">Converter, Distributor and Fabricator<br>of Pressure Sensitive Tapes, Elastomers,<br>Coated Fabrics, and Plastic Films</h2>
+					</div>
+
+					<div class="tp-caption sfb stt"
+						data-x="left" data-hoffset="15"
+						data-y="center" data-voffset="140" 
+						data-speed="800" data-start="1000"
+						data-endspeed="600" data-end="8000"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<a href="/about.php" class="btn-slider">About Us</a>
+					</div>
+
+					
+				</li>
+
+
+				<!-- SLIDE 4 -->
+				<li data-transition="fade" data-slotamount="7" data-delay="8000" data-title="Slide 4">
+					<!-- Background Image -->
+				   <img src="images/sample_4.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
+				   <div class="bg-overlay op5"></div>
+
+				   
+					<div class="tp-caption sfr stb"
+						data-x="right" data-hoffset="-15"
+						data-y="center" data-voffset="0" 
+						data-speed="900" data-start="600"
+						data-endspeed="400" data-end="7980"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h2 class="title-slider-large text-right">Premium Services</h2>
+					</div>
+
+					<div class="tp-caption sfr stb"
+						data-x="right" data-hoffset="-15"
+						data-y="center" data-voffset="70" 
+						data-speed="1000" data-start="1000"
+						data-endspeed="400" data-end="7900"
+						data-easing="Power3.easeInOut" data-endeasing=""
+						style="z-index: 4">
+						<h4 class="title-slider-small uppercased text-right">
+						Splitting<span class="py10"></span>
+						Die cutting<span class="py10"></span>
+						Laminating<br>
+						Belt Fabrication<span class="py10"></span>
+						Rapid Turn Prototypes<br>
+						Liquid Injection Molding
+						</h4>
+					</div>
+
+				</li>
+
+			</ul>
+		</div>
+	</div>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="title-border">Products</h2>
+				</div>
+			</div>
+			<div class="row col-p0 max_height sm_max_height">
+				<div class="col-sm-12 col-md-3">
+					<a href="high-performance-tapes.php">
+						<div class="box-services-d bg-img bg_home_services_high_performance">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">High Performance Tapes</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-3">
+					<a href="mil-spec-approved-tape.php">
+						<div class="box-services-d bg-img bg_home_services_milspec">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">MIL-SPEC Approved Tape<br>Rubber Materials</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-3">
+					<a href="coated-fabrics-films.php">
+						<div class="box-services-d bg-img bg_home_services_coated">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Coated Fabrics<br>Films</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-3">
+					<a href="silicone-elastomers.php">
+						<div class="box-services-d bg-img bg_home_services_silicone">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Silicone Rubber<br>Sponge<br>Elastomers</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="title-border">Capabilities</h2>
+				</div>
+			</div>
+			<div class="row col-p30 mb40">
+				<div class="col-sm-6 sm-box2">
+					<div class="box-services-b">
+						<div class="box-left"><i class="fal fa-repeat-alt fa-style2"></i></div>
+						<div class="box-right">
+							<a href="rewind-slitting.php"><h3 class="title-small">Rewind Slitting</h3></a>
+							<p class="mb10">This highly effective method of cutting tape offers tight, (+/-) .005 of an inch precision tolerances and crisp adhesive edges.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="box-services-b">
+						<div class="box-left"><i class="fal fa-scalpel fa-style2"></i></div>
+						<div class="box-right">
+							<a href="single-knife-slitting.php"><h3 class="title-small">Single Knife Slitting</h3></a>
+							<p class="mb10">Used for tapes, films, fabrics, and elastomers slit to width from a master roll measuring between .125 of an inch to 58 inches wide.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row col-p30 mb40">
+				<div class="col-sm-6 sm-box2">
+					<div class="box-services-b">
+						<div class="box-left"><i class="fal fa-compact-disc fa-style2"></i></div>
+						<div class="box-right">
+							<a href="rotary-die-cutting.php"><h3 class="title-small">Rotary Die Cutting</h3></a>
+							<p class="mb10">Rotary die cutting machinery produces pressure sensitive tapes, fabrics, films, and thin elastomers "kiss cut" to a client's particular shape or pattern.</p>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-sm-6">
+					<div class="box-services-b">
+						<div class="box-left"><i class="fal fa-ruler-triangle fa-style2"></i></div>
+						<div class="box-right">
+							<a href="steel-rule-die-cutting.php"><h3 class="title-small">Steel Rule Die Cutting</h3></a>
+							<p class="mb10">One of the most cost-effective ways to die cut elastomers measuring up to 30 inches in length and width, and 0.5 to 5 millimeters in thickness</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="row col-p30">
+				<div class="col-sm-6 sm-box2">
+					<div class="box-services-b">
+						<div class="box-left"><i class="fal fa-water fa-style2"></i></div>
+						<div class="box-right">
+							<a href="liquid-injection-molding.php"><h3 class="title-small">Liquid Injection Molding</h3></a>
+							<p class="mb10">A strong candidate for gaskets, seals, and cushioning pads in electronic portable and hand-held devices that need to function within challenging outdoor conditions.</p>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-sm-6">
+					<div class="box-services-b">
+						<div class="box-left"><i class="far fa-tint fa-style2"></i></i></div>
+						<div class="box-right">
+							<a href="water-jet-cutting.php"><h3 class="title-small">Waterjet Cutting</h3></a>
+							<p class="mb10">Accurately slice through thick, low durometer solid or sponge materials without the edge concavity usually associated with steel rule die cutting.</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="title-border">Industry Applications</h2>
+				</div>
+			</div>
+			<div class="row col-p0 max_height sm_max_height">
+				<div class="col-sm-12 col-md-4">
+					<a href="aircraft-aerospace-applications.php">
+						<div class="box-services-d bg-img bg_home_applications_aircraft">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Aircraft<br>Aerospace</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-4">
+					<a href="construction-windows-doors-applications.php">
+						<div class="box-services-d bg-img bg_home_applications_construction">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Construction<br>Windows<br>Doors</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-4">
+					<a href="electronics-electrical-assembly-applications.php">
+						<div class="box-services-d bg-img bg_home_applications_electronics">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Electronics<br>Electrical Assembly</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="row col-p0 max_height sm_max_height">
+				<div class="col-sm-12 col-md-4">
+					<a href="industrial-applications.php">
+						<div class="box-services-d bg-img bg_home_applications_industrial">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Industrial</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-4">
+					<a href="packaging-applications.php">
+						<div class="box-services-d bg-img bg_home_applications_packaging">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Packaging</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<? include 'inc_footer.php'; ?>
