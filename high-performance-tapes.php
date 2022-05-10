936a914e2479443c34bdbb52f0bbbc0242d09a33
Andreea Butiu <deea.b22@gmail.com>
2022-05-10 10:41:26 +0300

    Create high-performance-tapes.php
    initial

---
+++ high-performance-tapes.php
@@ -0,0 +1,109 @@
+<? $title = 'High Performance Tapes'; include 'inc_header.php'; ?>	
+
+	<section class="section-intro pb0">
+		<div class="container">
+			<div class="row mb30">
+				<div class="col-md-7 col-sm-8">
+					<h1 class=" mb20">High Performance Pressure Sensitive Tapes</h1>
+					<p class="mb30">Advanced Technology Supply, Inc. has a comprehensive line of pressure sensitive tapes, and a complete array of fabrication capabilities including single knife and rewind slitting, rotary die cutting, and liner and adhesive application. These capabilities permit us to stock bulk inventory of tape "logs", "jumbos", liner stock, and all the essentials needed to complete your order per your specification.</p>				
+				</div>
+				<div class="col-md-5 col-sm-3">
+					<img class="blog-post-image" src="/images/p0959.png" alt="High Performance Tapes" title="High Performance Tapes"/>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="section-bg bg-img bg03 stellar color-white mb0" data-stellar-background-ratio="0.4" style="background-position: 50% 0px;">
+		<div class="bg-overlay op8"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					<ul class="unstyled row">
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Teflon</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Die Cut</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Military Specification</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Aircraft</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Leading Edge</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Pressure Sensitive</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Silicone</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Conformal Coat Masking</li>
+				          <li class="col-md-4 col-xs-6 mb20"><i class="fa fa-chevron-right fs-small color-main mr10"></i>Conformal Masking Dots</li>
+			        </ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="page-clients page-clients-b section-bg mt0 mb0" style="background: #fff;">
+		<div class="container">
+			<div class="row"> 
+				<div class="col-md-12">				
+				
+				<article class="blog-post-content">
+
+						<h3 class="title-small mb40">ATS is an approved converter / fabricator of several manufacturers,<br/> with access to numerous manufacturers.</h3>
+
+				</article>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/saint-gobain.png" alt="Saint-Gobain">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/3m.png" alt="3M">
+				</div>		
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/afc.png" alt="AFC">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/breyden.jpg" alt="Breyden">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/dewal.jpg" alt="Dewal">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/nitto.jpg" alt="Nitto">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/scapa.png" alt="Scapa">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/stockwell.png" alt="Stockwell Elastomerics">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/rogers.png" alt="Rogers Corporation">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/saylor.png" alt="Saylor">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					<img src="images/vendors/sheldahl.png" alt="Sheldahl">
+				</div>
+				<div class="col-xs-6 col-sm-3 xs-box">
+					Many Others...
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<style>
+	.xs-box { height:120px; }
+	.xs-box img { max-height:70px; -webkit-filter: grayscale(100%); filter: grayscale(100%); }
+	</style>
+	
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h3 class="title-small">While there are often overlaps in these product lines, each vendor has certain products, which are recognized as the best available. Our goal is to assist you in picking the best product for your needs, at the best price, and in most cases, ship same day.</h3>
+				</div>
+			</div>
+		</div>
+	</section> 
+
+
+<? include 'inc_footer.php'; ?>
+
+
+
