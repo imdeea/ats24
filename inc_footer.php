c4cae4406790b8c8b1881aafa94900d0b30287c5
Andreea Butiu <deea.b22@gmail.com>
2022-06-03 15:57:30 +0300

    Badges and social media links

--- inc_footer.php
+++ inc_footer.php
@@ -29,13 +29,27 @@
 						<i class="fa fa-phone"></i> &nbsp; <a href="tel:+1-800-793-6307">800.793.6307</a><br>
 						<i class="fa fa-print"></i> &nbsp; 407.880.3338<br>
 						<i class="fa fa-envelope"></i> &nbsp; <a href="mailto:sales@ats24.com">sales@ats24.com</a>
+
+						<div class="social-media">
+							<ul>
+								<li><a href="https://www.linkedin.com/company/advanced-technology-supply-inc-of-orlando"><i class="fab fa-linkedin"></i></a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 				<div class="col-sm-12 col-md-4">
 					<div class="footer-widget">
 						<h3 class="footer-title">Certifications</h3>
 						<div class="sidebar-tweet clearfix">
-							<a href="/pdf/iso-certificate-of-reg-2019.pdf" target="_blank" onclick="LogClick(this);"><img src="/images/nqa_hi_res_logo.png" alt="NNQA - ISO 9001:2015 and AS9100:2016 Certified" title="NQA - ISO 9001:2015 and AS9100:2016 Certified" style="max-width:125px;" width="581" height="519"></a>
+							<div class="row">
+								<div class="col-md-5" style="padding:20px; text-align: center">
+									<a href="/pdf/iso-certificate-of-reg-2019.pdf" target="_blank" onclick="LogClick(this);"><img src="/images/nqa_hi_res_logo.png" alt="NNQA - ISO 9001:2015 and AS9100:2016 Certified" title="NQA - ISO 9001:2015 and AS9100:2016 Certified" style="max-width:100%;" width="581" height="519"></a>
+									<img src="/images/iso-cert.png" alt="ISO Certified" title="ISO Certified" style="max-width:100%;" width="350" height="131">
+								</div>
+								<div class="col-md-7"  style="padding:10px; text-align: center">
+									<a href="https://anab.ansi.org/"><img src="/images/ANAB_Accredited.png" alt="ANAB Accredited" title="ANAB Accredited" style="max-width:100%" width="860" height="695"></a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -50,7 +64,8 @@
 				</div>
 				*/ ?>
 				<div class="col-sm-12 col-md-12">
-					<img src="/images/ConverterSelect.png" width="1092" height="297" style="height:50px; width: auto; display: inline-block;" /> 
+					<img src="/images/ConverterSelect.png" width="1092" height="297" style="height:50px; width: auto; display: inline-block;" />
+					<a id='COCBadgeURL' href='https://www.chamberofcommerce.com/united-states/florida/deerfield-beach/wholesaler/2006473472-advanced-technology-supply-inc?source=memberwebsite' target='_tab'><img style='width:auto;height:80px; margin-left: 10px' id='COCBadgeIMG' src='https://coc.codes/images/badge/2006473472'></a>
 					<p class="copyright">&copy; Copyright <?=date('Y')?></p>
 				</div>
 			</div>
