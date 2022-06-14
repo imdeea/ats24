a61be89948e05900790e6aa918d36bc5d4d4e653
Andreea Butiu <deea.b22@gmail.com>
2022-06-14 10:18:56 +0300

    New page

---
+++ rapid-turn-prototyping.php
@@ -0,0 +1,107 @@
+<?
+$title = 'Rapid Turn Prototyping';
+$meta_descr = 'Advanced Technology Supply, Inc offers rapid turn prototyping services to companies who are looking to test various designs, materials, and product sizes before finalizing their die-cut solution with us. Get a FREE quote!';
+$schema = "<script type='application/ld+json'>
+{
+  \"@context\": \"http://www.schema.org\",
+  \"@type\": \"service\",
+  \"brand\": \"Advanced Technology Supply, Inc\",
+  \"url\": \"https://ats24.com/rotary-die-cutting.php\",
+  \"name\": \"Rotary Die Cutting Services\",
+  \"image\": \"https://ats24.com/images/rotary-die-cutting.png\",
+  \"description\": \"Advanced Technology Supply, Inc provides bespoke, precision rotary die-cutting services for a wide range of materials. MilItary-spec\n\"
+}
+ </script>";
+include 'inc_header.php'; ?>
+
+	<section class="section-intro pb0">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5 offset-md-1 col-md-push-7">
+					<img class="blog-post-image" src="/images/rapid-turn-prototyping.jpg" alt="Rapid Turn Prototyping" title="Rapid Turn Prototyping" width="450" height="338">
+                    <h3 class="mt30">Advantages of Rapid Prototyping :</h3>
+                    <ul>
+                        <li>The Prototype gives a future snapshot of how the final product will look .</li>
+                        <li>Detect design flaws or deficiencys in the early stages of development so they can be rectified in time.</li>
+                        <li>It is cost effective, clients are not financially commited to final product</li>
+                        <li>Development time and cost reduces.</li>
+                        <li>Can give with concept proof that would be needed for attracting funds.</li>
+                    </ul>
+				</div>
+				<div class="col-md-6 col-md-pull-5">
+					<h1 class=" mb20">Rapid Turn Prototyping</h1>
+                    <p>Advanced Technology Supply, Inc offers rapid turn prototyping services to companies who are looking to test various designs, materials, and product sizes before finalizing their die-cut solution with us. We havevarious converting options to ensure your design will meet your exact specifications with a rapid turnaround, minimal lead times and setup costs.</p>
+                    <p>Rapid prototyping allows companies to cost-effectively test the efficiency of a part or product design before they are committed to a big financial overlay when the product is manufactured in large quantities.</p>
+                    <p>It is also very useful for digitally creating a three-dimensional model of a part or product, so the client can see a sample of what the final outcome will be.</p>
+                    <p>Our digital converting solutions for prototyping are:</p>
+                    <ul>
+                        <li><strong>Laser Cutting</strong></li>
+                        <li><strong>Waterjet/Abrasivejet</strong></li>
+                        <li><strong>Die Cutting</strong></li>
+                    </ul>
+                    <h3 class="mt30">Why would companies want to use rapid turn prototyping?</h3>
+                    <p>The simple answer is <strong>Testing.</strong></p>
+                    <p>It&rsquo;s not uncommon for the final product to not be made of the same materialas the prototype, therefore prototypes are more focused on the shape or size of a design, rather than its strength or durability.</p>
+                    <p>The word &lsquo;rapid&rsquo; is exactly that. At ATS we can take your idea or drawing and rapidly convert it into the right end product.</p>
+                    <h3 class="mt30">The Process</h3>
+                    <p>We recognize that every step along the prototyping journey needs to be done correctly. Once you have an idea or design in mind, our team of professionals will advise you on what will be the best way to achieve your end product with the different materials, explain our process and production run, and how our machinery is able to reduce both costs and waste.</p>
+                    <p class="mb30">If you&rsquo;re looking for help to turn your idea into reality, with a high-quality, cost-effective product at rapid speed, contact ATS today to receive a quote.</p>
+					<div class="col-sm-12 col-md-12 text-right mt30">
+						<a href="request-quote.php" class="btn btn-icon btn-a" style="text-transform: uppercase; font-size: 20px;"><i class="fa fa-file-invoice-dollar" style="font-size: 25px"></i> Request a Quote</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="title-border">Portfolio</h2>
+				</div>
+			</div>
+			<div class="row col-p0 max_height sm_max_height">
+				<div class="col-sm-12 col-md-4">
+					<a href="custom-rewinding-ptfe-tape-rolls.php">
+						<div class="box-services-d bg-img" style="background-image: url(images/custom-slitting-rewinding01.jpg)">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Custom Rewinding of PTFE Tape Rolls for the Aircraft Industry</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-4">
+					<a href="custom-slitting-rewinding-ptfe-tape-rolls.php">
+						<div class="box-services-d bg-img" style="background-image: url(images/overhaul-industry01.jpg)">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Custom Slitting & Rewinding of PTFE Tape Rolls for the Aircraft Repair and Overhaul Industry</h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-sm-12 col-md-4">
+					<a href="custom-die-cutting-polyester-masking-tape.php">
+						<div class="box-services-d bg-img"  style="background-image: url(images/custom-die-cutting01.jpg)">
+							<div class="bg-overlay"></div>
+							<div class="row col-p0">
+								<div class="col-sm-12">
+									<h3 class="title-medium br-bottom">Custom Die Cutting of Polyester Masking Tape for the Electronics Industry  </h3>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+<? include 'inc_footer.php'; ?>
